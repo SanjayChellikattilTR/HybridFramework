0a2cceebe305a833deaad5477e937f82ee99b0d5
SanjayChellikattilTR <sanjay.chellikattil@thomsonreuters.com>
2024-09-25 10:35:43 +0530

    Login test, uilogin,AutomationWrapper

--- MedicalRecord/UiLoginTest.cs
+++ MedicalRecord/UiLoginTest.cs
@@ -1,26 +1,40 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
-
-
-namespace ThomsonReuters.MedicalRecord
+namespace ThomsonReuters.MedicalRecordAutomation
 {
-    public class LoginUiTest
+    /// <summary>
+    /// Login UI Test Cases
+    /// </summary>
+    public class LoginUITest
     {
-        [Test]
-        public void TitleTest()
+        IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTestMethod()
         {
-            IWebDriver driver = new ChromeDriver();
+            driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
 
             driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
+        }
 
+        [TearDown]
+        public void AfterTestMethod()
+        {
+            driver.Dispose();
+        }
+
+
+        [Test]
+        public void TitleTest()
+        {
             string actualTitle = driver.Title;
             Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
         }
@@ -28,16 +42,9 @@ namespace ThomsonReuters.MedicalRecord
         [Test]
         public void ApplicationDescriptionTest()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
-
-            driver.Navigate().GoToUrl("https://demo.openemr.io/b/openemr");
-
-            string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
-
             //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
+            string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
+            Assert.That(actualDescription, Is.EqualTo("The most popular open-source Electronic Health Record and Medical Practice Management solution."));
         }
-
     }
 }
